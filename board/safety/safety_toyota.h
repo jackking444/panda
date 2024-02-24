e1f1d02e358afca3f8614ae604a1c0f1a5167c96
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2024-02-23 19:32:44 -0700

    auto break hold

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -339,6 +339,12 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
         }
       }
     }
+    // DP: auto break hold https://github.com/AlexandreSato
+    if (addr == 0x344 && (alternative_experience & ALT_EXP_ALLOW_AEB)) {
+      if(vehicle_moving || gas_pressed || !acc_main_on) {
+        tx = false;
+      }
+    }
   }
 
   // UDS: Only tester present ("\x0F\x02\x3E\x00\x00\x00\x00\x00") allowed on diagnostics address
@@ -403,7 +409,9 @@ static int toyota_fwd_hook(int bus_num, int addr) {
     int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
     // in TSS2 the camera does ACC as well, so filter 0x343
     int is_acc_msg = (addr == 0x343);
-    int block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal);
+    // DP: Block AEB when stoped to use as a automatic brakehold
+    int is_aeb_msg = (addr == 0x344 && (alternative_experience & ALT_EXP_ALLOW_AEB));
+    int block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal) || (is_aeb_msg && !vehicle_moving && acc_main_on && !gas_pressed);
     if (!block_msg) {
       bus_fwd = 0;
     }
